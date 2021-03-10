2a6a516bed77e99c596b66a2791c696c2bc2a3bd
Gabriel Mendonca <gabriel.dev.lop@gmail.com>
2021-03-10 13:14:40 -0300

    [FEAT][CREATING HEADER STYLE]

---
+++ src/components/Header/style.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    height: 70,
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+  },
+  imageBox: {
+    height: 24,
+    width: 24,
+    borderRadius: 100,
+    backgroundColor: 'black'
+  },
+})
