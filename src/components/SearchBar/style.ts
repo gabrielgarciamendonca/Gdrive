832fd4109c16d8a407a39f9a88f86c27b9d60bf2
Gabriel Mendonca <gabriel.dev.lop@gmail.com>
2021-03-10 13:31:57 -0300

    [FEAT][STYLING SEARCHBAR COMPONENT]

---
+++ src/components/SearchBar/style.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    height: 56,
+    backgroundColor: '#FFFFFF',
+    marginTop: 30,
+    borderRadius: 8,
+  }
+})
