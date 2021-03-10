70cb2a784cf46c133078bcc3836e41a62c3db371
Gabriel Mendonca <gabriel.dev.lop@gmail.com>
2021-03-10 13:32:13 -0300

    [FEAT][CREATING SEARCHBAR COMPONENT]

---
+++ src/components/SearchBar/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+
+import { styles } from './style';
+
+const SearchBar: React.FC = () => {
+  return (
+    <SafeAreaView style={ styles.container }>
+
+    </SafeAreaView>
+  );
+}
+
+export default SearchBar;
