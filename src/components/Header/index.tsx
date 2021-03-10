a88f7bd969d81b567e26492e7572c9c1da37478b
Gabriel Mendonca <gabriel.dev.lop@gmail.com>
2021-03-10 13:15:09 -0300

    [FEAT][ADDING POPPINS FONT]

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { SafeAreaView, Text, View } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import { useFonts, Poppins_500Medium } from '@expo-google-fonts/poppins';
+
+import { styles } from './style';
+import { Launchs }  from '../../assets/menu-button';
+
+const Header: React.FC = () => {
+
+  let [fontsLoaded] = useFonts({
+    Poppins_500Medium,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <SafeAreaView style={ styles.container }>
+      <Launchs />
+      <Text style={{ fontFamily: 'Poppins_500Medium', color: '#21205A', fontSize: 16 }}>My Drive</Text>
+      <View style={ styles.imageBox }></View>
+    </SafeAreaView>
+  );
+}
+
+export default Header;
