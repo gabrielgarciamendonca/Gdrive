2e91e9f580fc69500482f1def1150dda43f89068
Gabriel Mendonca <gabriel.dev.lop@gmail.com>
2021-03-10 18:48:07 -0300

    [FEAT][CREATING SEARCHBAR TEMPLATE]

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView, TextInput } from 'react-native';
 
 import { styles } from './style';
+import { Toogle }  from '../../assets/toogle-icon';
 
 const SearchBar: React.FC = () => {
   return (
     <SafeAreaView style={ styles.container }>
-
+      <Toogle />
+      <TextInput style={ styles.input } placeholder="Search Files" />
     </SafeAreaView>
   );
 }
