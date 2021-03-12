bb097192532025627cd6f470b0b259fed6f6270c
Gabriel Mendonca <gabriel.dev.lop@gmail.com>
2021-03-11 21:42:12 -0300

    [CREATE][FILTER BACKGROUND TEMPLATE]

--- src/screens/Home/style.ts
+++ src/screens/Home/style.ts
@@ -2,9 +2,16 @@ import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
   container: {
-    margin: 10,
+    
   },
   foldersContainer: {
-    
+    backgroundColor: '#FFF',
+    borderTopRightRadius: 16,
+    borderTopLeftRadius: 16,
+    paddingHorizontal: 10,
+    paddingVertical: 25,
+  },
+  menuContainer: {
+    margin: 10,
   }
 })
