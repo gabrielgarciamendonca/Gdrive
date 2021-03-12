5da4d15974977746c843ce2082e7752d86edfb3f
Gabriel Mendonca <gabriel.dev.lop@gmail.com>
2021-03-11 23:10:55 -0300

    [CHANGE][POSITION OF FOLDERS IN HOME]

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { SafeAreaView, View} from 'react-native';
+import { SafeAreaView, View, ScrollView } from 'react-native';
 import CardStorage from '../../components/CardStorage';
 import Filter from '../../components/Filter';
+import Folder from '../../components/Folder';
 import Header from '../../components/Header';
 import SearchBar from '../../components/SearchBar';
 
@@ -9,16 +10,23 @@ import { styles } from './style';
 
 const Home: React.FC = () => {
   return (
-    <SafeAreaView style={ styles.container }>
+    <ScrollView style={ styles.container }>
       <View style={ styles.menuContainer }>
         <Header />
         <SearchBar />
         <CardStorage />
       </View>
       <View style={ styles.foldersContainer }>
-        <Filter />
+        <View style={ styles.filter }>
+          <Filter />
+        </View>
+        <View style={ styles.folders }>
+          <Folder title="Web Design" date="20 Dec 2020"/>
+          <Folder title="App Design" date="21 Dec 2020"/>
+          <Folder title="Font Collections" date="22 Dec 2020"/>
+        </View>
       </View>
-    </SafeAreaView>
+    </ScrollView>
   );
 }
 
