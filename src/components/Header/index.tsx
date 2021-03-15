54305a8849d708eba97d0dbc35d4e06331a72ca6
Gabriel Mendonca <gabriel.dev.lop@gmail.com>
2021-03-15 14:19:29 -0300

    [CREATE][HEADER DINAMIC ELEMENTS]

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,11 +2,21 @@ import React from 'react';
 import { SafeAreaView, Text, View } from 'react-native';
 import AppLoading from 'expo-app-loading';
 import { useFonts, Poppins_500Medium } from '@expo-google-fonts/poppins';
+import { AntDesign } from '@expo/vector-icons';
 
 import { styles } from './style';
 import { Menu }  from '../../assets/menu-button';
+import { TwoPoints } from '../../assets/two-points-icon';
+import { useNavigation } from '@react-navigation/native';
 
-const Header: React.FC = () => {
+interface HeaderProps {
+  title: string;
+  showCancel: boolean;
+}
+
+export default function Header(props: HeaderProps) {
+
+  const navigation = useNavigation();
 
   let [fontsLoaded] = useFonts({
     Poppins_500Medium,
@@ -16,13 +26,23 @@ const Header: React.FC = () => {
     return <AppLoading />;
   }
 
+  function fallBack() {
+    navigation.goBack();
+  }
+
   return (
     <SafeAreaView style={ styles.container }>
-      <Menu />
-      <Text style={{ fontFamily: 'Poppins_500Medium', color: '#21205A', fontSize: 16 }}>My Drive</Text>
-      <View style={ styles.imageBox }></View>
+      {props.showCancel ? (<>
+        <AntDesign name='left' size={ 24 } color='#21205A' onPress={ () => { fallBack() } }/>
+        <TwoPoints />
+      </>) :
+      (<>
+        <Menu />
+        <Text style={{ fontFamily: 'Poppins_500Medium', color: '#21205A', fontSize: 16 }}>{ props.title }</Text>
+        <View style={ styles.imageBox }></View>
+      </>)
+      }
+      
     </SafeAreaView>
   );
 }
-
-export default Header;
