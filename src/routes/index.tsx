c8f0cb8009bc279393922341183517b062c413dc
Gabriel Mendonca <gabriel.dev.lop@gmail.com>
2021-03-18 11:49:56 -0300

    [ADD][INTRODUCING CUSTOM DRAWER]

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,29 @@
-import { createStackNavigator } from '@react-navigation/stack';
+// import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import Home from '../screens/Home';
 import DetailStorage from '../screens/DetailStorage';
 import React from 'react';
 import Header from '../components/Header';
+import { useWindowDimensions } from 'react-native';
+import SideBar from '../screens/SideBar';
 
-const { Navigator, Screen } = createStackNavigator();
+const { Navigator, Screen } = createDrawerNavigator();
 
 export default function Routes() {
+  const dimensions = useWindowDimensions();
+  const isLargeScreen = dimensions.width >= 768;
   return (
     <Navigator
       screenOptions={
         { headerShown: false }
       }
+      drawerStyle={
+        {
+          ...isLargeScreen ? null : { width: '100%' }, 
+          backgroundColor: '#4378DB',
+        }
+      }
+      drawerContent={(props) => <SideBar {...props} />}
     >
       <Screen
         name='Home'
