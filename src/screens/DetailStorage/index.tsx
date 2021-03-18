791318c5b52deb35ab598d38b653204131df116c
Gabriel Mendonca <gabriel.dev.lop@gmail.com>
2021-03-18 18:32:59 -0300

    [ADD][INCLUDE TOTAL CAPACITY]

--- src/screens/DetailStorage/index.tsx
+++ src/screens/DetailStorage/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { SafeAreaView, Text } from 'react-native';
+import TotalCapacity from '../../components/TotalCapacity';
 
 export default function DetailStorage() {
   return (
     <SafeAreaView>
       <Text>
-        test
+        <TotalCapacity />
       </Text>
     </SafeAreaView>
   )
